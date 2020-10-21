bc489d77c102a158d2339e7f32a14f04045b5854
unknown <291247908@qq.com>
2020-10-21 15:37:15 +0800

    file.txt记录节点区块信息,node2.txt node3.txt,transcation.txt记录经转换后的数据，tanscation.json记录节点交易信息，此外static与templates是py程序flask框架所需要的一些文件. 本次仍为测试版本.

--- ceshi.py
+++ ceshi.py
@@ -5,24 +5,29 @@ import socket
 import threading
 import os
 import time
+import random
 from os.path import join as pjoin
+from argparse import ArgumentParser
+from flask import Flask,render_template
 from web3 import Web3,HTTPProvider,WebsocketProvider
-from web3.auto import w3
 
+server1 = '127.0.0.1:3344'
+server2 = '127.0.0.1:5010'
+server3 = '127.0.0.1:8545'
 
 class Client():
 
     def __init__(self,ip,rpcport):
-        self.server_address = ('127.0.0.1', 8000)
+        self.server_address = ('172.20.0.2', 8000)
         self.ww = Web3(HTTPProvider('http://' + ip + ':' + rpcport, request_kwargs={'timeout': 60}))
         self.ad = self.ww.geth.admin.datadir()
         self.info = self.ww.geth.admin.node_info()
         self.num = self.ww.eth.getBlock("latest")['number']
         print(self.ww.geth.admin.datadir())
+        self.port = None
         self.ip = None
-        # 链接标志
+        # 链接标志pip
         self.connect_flag = False
-
     def recv_msg(self):
         print("正在连接服务器....")
 
@@ -44,38 +49,61 @@ class Client():
                         if self.ww.isConnected() == True:
                             print('与区块链节点已连接....')
                             # print(self.info['id'])
+                            print(self.port)
                             self.client_socket.sendall(bytes(repr(json.dumps(self.info['enode'])).encode('utf-8')))
-
+                            # data1 = []
+                            # data = change_rcg(self.port, port1)
+                            # data1.append(data)
+                            # path1 = r"C:\Users\cao\Desktop\runoob01\blockchain\Readfile\transcation.txt"
+                            # text_save(path1,data1)
                             while True:
-                                if  self.ww.eth.getBlock('latest')['number'] >self.num:
+                                if  self.ww.eth.getBlock('latest')['number']  >self.num :
                                     self.client_socket.sendall(bytes(repr(json.dumps(self.ww.eth.getBlock('latest')['timestamp'])).encode('utf-8')))
+                                    if self.ww.eth.getBlock("latest")['miner'] != self.ww.eth.accounts[0]:
+                                        re_timestamp = time.time()
+                                        se_timestamp = self.ww.eth.getBlock("latest")['timestamp']
+                                        data1 = []
+                                        port1 = (random.randrange(9) + 1) * 800 + 1000
+                                        data = change_rcg(self.port, port1,se_timestamp,re_timestamp)
+                                        data1.append(data)
+                                        if self.port >=3000 and self.port<5000:
+                                            path1 = r"transcation.txt"
+                                        elif self.port>=5000 and self.port<8000:
+                                            path1 = r"node2.txt"
+                                        else:
+                                            path1 = r"node3.txt"
+                                        text_save(path1, data1)
+                                    # self.client_socket.sendall(bytes(repr(json.dumps(self.ww.eth.getBlock('latest')['timestamp'])).encode('utf-8')))
                                     self.num = self.ww.eth.getBlock('latest')['number']
+
                                     transaction = self.ww.eth.getBlock("latest")['transactions']
-                                    print(transaction)
-                                    # self.client_socket.sendall(bytes(repr(json.dumps(
-                                    #     self.ww.eth.getTransaction(
-                                    #         "0xac35a4d4511a8c02d45b005880c4865c24516a8c96462312b5475db8eb57fcf4")['from'])).encode('utf-8')))
                                     reponse = {
                                         'block_num':self.num,
                                         'timestamp':self.ww.eth.getBlock('latest')['timestamp'],
                                         'transaction':self.ww.eth.getBlock('latest')['transactions'],
                                         'ip':self.ww.geth.admin.node_info()['ip'],
                                     }
-                                    path1 = r"C:\Users\cao\Desktop\runoob01\blockchain\Readfile"
                                     if transaction :
+                                        # 转换数据
+                                        data1=[]
+                                        port1 = (random.randrange(9)+1) *900 + 1000
+                                        data = change_rcg(self.port,port1)
+                                        data1.append(data)
                                         reponse1 = {
                                             'send_ip':self.ip,
-                                            'recieve_ip':transaction['to'],
+                                            'recieve_ip':server1,
                                             'timestamp':self.ww.eth.getBlock(transaction['blockNumber'])['timestamp'],
                                         }
-                                        listdir1 = os.listdir(path1)
+                                        path = r"./"
+                                        listdir1 = os.listdir(path)
                                         if 'transcation.json' in listdir1:
-                                            f = open(pjoin(path1, 'transcation.json'), 'a')
-                                            f.write(json.dumps(reponse1, ensure_ascii=False, indent=4))
+                                            f = open(pjoin(path, 'transcation.json'), 'a')
+                                            f.write(json.dumps(data1, ensure_ascii=False, indent=4))
                                             f.close()
-                                    listdir = os.listdir(path1)
-                                    if 'file.json' in listdir:
-                                        f = open(pjoin(path1, 'file.json'), 'a')
+                                    path = r"./"
+                                    listdir = os.listdir(path)
+                                    if 'file.txt' in listdir:
+                                        f = open(pjoin(path, 'file.txt'), 'a')
                                         f.write(json.dumps(reponse, ensure_ascii=False, indent=4))
                                         f.close()
                                     continue
@@ -91,8 +119,9 @@ class Client():
                         recv_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                         print( '服务器 ' + recv_time + ':\n')
                         print(' ' + msg_recv + '\n')
+
             except:
-                print('连接服务器失败')
+                print('与服务器断开连接...')
                 break
 
     def start_new_thread(self):
@@ -128,19 +157,63 @@ class Client():
         timestamp = time.time()
         return information,timestamp
 
+def change_rcg(port1,port2,t1,t2):
+    date=[]
+    name1={}
+    name2={}
+    city_name = ['包头','福州','海口','乌鲁木齐','保定','兰州','中山','丹东','北海','南京','南宁','南昌','南通','金华','湘潭','秦皇岛','绍兴','衢州']
+    value = int(abs(t2-t1)*6)
+    if(int(port1)<5000):
+        name1['name'] = '北京'
+    elif(int(port1)>=5000 and int(port1)<8000):
+        name1['name'] = '上海'
+    else:
+        name1['name'] = '广州'
+    # if (int(port2) < 5000):
+    #     name2['name'] = '包头'
+    #     name2['value'] = value
+    # elif (int(port2) >= 5000 and int(port2) < 8000):
+    #     name2['name'] = '福建'
+    #     name2['value'] = value
+    # else:
+    #     name2['name'] = '海口'
+    #     name2['value'] = value
+    num = int((port2 - 1800)/450)
+    name2['name'] = city_name[num]
+    name2['value'] = value
+
+    date.append(name1)
+    date.append(name2)
+    return date
+
+
+def text_save(filename, data):#filename为写入CSV文件的路径，data为要写入数据列表.
+    file = open(filename,'a')
+    for i in range(len(data)):
+        s = str(data[i]).replace('[','').replace(']','')+','+'\n'#去除[],这两行按数据不同，可以选择
+        # s = s.replace("'",'').replace(',','') +'\n'   #去除单引号，逗号，每行末尾追加换行符
+        file.write(s)
+    file.close()
+    print("保存成功",filename)
+
 def main():
-    ip = input("请输入要输入的ip:")
-    rpcport = input("请输入要输入的端口号:")
-    wf = Client(ip,rpcport)
-    wf.ip = str(ip)+":"+str(rpcport)
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=3965, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = str(args.port)
+    ip = '192.168.43.188'
+    wf = Client(ip,port)
+    wf.port = int(port)
+    wf.ip = str(ip)+":"+str(port)
     wf.start_new_thread()
+
 
 if __name__ == '__main__':
 
     main()
 
     while True:
-        a = input("请输入要发送的信息:")
+        a = input()
 
 
 
--- server1.py
+++ server1.py
@@ -1,53 +1,28 @@
-import tkinter as tk
-import tkinter.font as tkfont
-import tkinter.messagebox
-import sys
+import os
 import socket
 import time
 import threading
 import json
+from flask import Flask,render_template,jsonify
 from threading import Thread
 
 class Server():
     def __init__(self):
-        self.server_window = tk.Tk()
-        self.font_style = tkfont.Font(family='Microsoft YaHei UI', size=13)
         self.g_conn_pool = {}  # 连接池
         # 记录客户端数量
         self.num =0
         # 服务器本地地址
-        self.address = ('127.0.0.1', 8000)
+        self.address = ('0.0.0.0', 8000)
         self.g_socket_server = None
         self.g_conn_pool = []
         # 初始化服务器
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind(self.address)
-        self.server_socket.listen(128)
+        self.server_socket.listen(5)
 
 
-    def init_window(self):
-        """初始化客户端窗口"""
-        self.server_window.title("Python  Server")
 
-        # 设置窗口大小为480x520px
-        self.server_window.geometry('480x520')
-        # 接收消息的listbox框
-        self.output_frame = tk.Listbox(self.server_window, font=('',9))
-        self.output_frame.place(x=10, y=10,relwidth=1,relheight=0.7, anchor='nw')
-        # 滚动条
-        self.sc = tk.Scrollbar(self.output_frame,command=self.output_frame.yview)
-        self.sc.pack(side=tk.RIGHT, fill=tk.Y)
-        self.output_frame.config(yscrollcommand=self.sc.set)
-
-        # 关闭按钮
-        self.close_button = tk.Button(self.server_window, text='关闭', font=self.font_style,
-                                      width=8, command=self.close_window)
-        self.close_button.place(x=50, y=475, anchor='nw')
-
-
-    def close_window(self):
-        """关闭窗口"""
-        sys.exit()
 
     def accept_client(self):
         """
@@ -62,7 +37,7 @@ class Server():
 
     def recv_msg(self,client,info):
         # 提示服务器开启成功
-        self.output_frame.insert(tk.END, '服务器已准备就绪！')
+        print('服务器已准备就绪！')
         client.sendall("connect server successfully!".encode(encoding='utf8'))
         # 持续接受客户端连接
         while True:
@@ -73,14 +48,14 @@ class Server():
                     if not msg_recv:
                         continue
                     elif msg_recv == 'Success':
-                        self.output_frame.insert(tk.END, '服务器与客户端成功建立连接...')
+                        print('服务器与客户端成功建立连接...')
                         self.client_socket.send(b'Success')
                     else:
                         recv_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                        self.output_frame.insert(tk.END, '客户端 ' + recv_time + ':\n')
-                        self.output_frame.insert(tk.END, ' ' + msg_recv + '\n')
+                        print('客户端 ' + recv_time + ':\n')
+                        print(' ' + msg_recv + '\n')
             except:
-                self.output_frame.insert(tk.END, '客户端断开连接...')
+                print('客户端断开连接...')
                 break;
 
     def remove_client(self,client_type):
@@ -96,12 +71,68 @@ class Server():
         thread.setDaemon(True)
         thread.start()
 
-def main():
-    py_server = Server()
-    py_server.init_window()
-    py_server.start_new_thread()
-    py_server.server_window.mainloop()
+#实例化一个Flask节点
+app = Flask(__name__)
+
+py_server = Server()
+
+py_server.start_new_thread()
+
+
+# 数据转换方法
+def change_msg(path):
+    data = []
+    with open(path, 'r') as f:
+        for line in f:
+            data1 = []
+            dic = {}
+            line = line.strip()
+            line.strip('[]')
+            line = line.strip().strip('[],')
+            line = line.split(',', 1)
+            # print(line)
+            if line[0]:
+                dic1 = {}
+                a = line[0].strip('{}').replace("'", "")
+                b = line[1].replace("'", "").replace("{", "").strip('}')
+                a1 = a.split(':', 1)
+                b1 = b.split(',', 1)
+                b2 = b1[0].split(':', 1)
+                b3 = b1[1].split(':', 1)
+                dic1[b2[0].replace("'",'').strip(" ")] = b2[1].replace("'",'').strip(" ")
+                dic1[b3[0].replace("'",'').strip(" ")] = b3[1].replace("'",'').strip(" ")
+                print(dic1)
+                if a != '':
+                    dic[a1[0].strip(" ")] = a1[1].strip(" ")
+                    data1.append(dic)
+                data1.append(dic1)
+                #print(str(data1))
+                data.append(data1)
+        return data
+
+
+@app.route('/map',methods=["post","get"])
+def showmap():
+    p = os.getcwd()
+    path2 = p+"/node2.txt"
+    path1 = p+"/transcation.txt"
+    path3 = p+"/node3.txt"
+    data = change_msg(path1)
+    data1 = change_msg(path2)
+    data2 = change_msg(path3)
+    return render_template('ss.html',result_data=data,result_data1=data1,result_data2=data2)
+
+@app.route('/')
+def hello():
+    return render_template("/visualserver/地图.html")
 
 
 if __name__ == '__main__':
-    main()
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5030, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0',port=port,threaded=True)
